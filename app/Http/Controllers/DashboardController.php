856e9f0264ed21022ba69d7998568d65a6b9c1f1
Doni Nurramdan <doni.nurramdan@indocyber.id>
2024-03-21 12:09:31 +0700

    add controlle ajax get data temperature

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
+use App\Models\TemperatureData;
 
 use Illuminate\Http\Request;
 
@@ -9,10 +10,21 @@ class DashboardController extends Controller
 {
     public function dashboard(){
         if(session("status_login")){
-
+            // echo now();
+            // var_dump($temperatureDatas);
+            // $temperatureDatas = TemperatureData::with('chamber', 'client')->get();
+            // echo json_encode($temperatureDatas);
             return view("dashboard");
         }else{
             return redirect("login");
         }
+    }
+    public function getdataBarChart(Request $request){
+        // Dapatkan data dari request POST
+        $clientId = $request->input('client_id');
+        $tanggal = $request->input('tanggal');
+
+        $temperatureDatas = TemperatureData::where('created_at','like',$tanggal)->where('id_client', $clientId)->with('chamber', 'client')->get();
+        echo json_encode($temperatureDatas);
     }
 }

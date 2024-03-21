cf5e0133153d5559e0aa889bde680a8fd7726178
Doni Nurramdan <doni.nurramdan@indocyber.id>
2024-03-21 10:10:12 +0700

    create seeder chamber

---
+++ database/seeders/ChamberSeeder.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ChamberSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        DB::table('chambers')->insert([
+            [
+                'nama_chamber' => 'Chamber 1',
+                'keterangan' => 'Keterangan untuk Chamber 1',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'created_by' => 1, // Asumsi user dengan ID 1 adalah yang membuat
+                'updated_by' => 1  // Asumsi user dengan ID 1 adalah yang terakhir mengupdate
+            ],
+            [
+                'nama_chamber' => 'Chamber 2',
+                'keterangan' => 'Keterangan untuk Chamber 2',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'created_by' => 1, // Asumsi user dengan ID 1 adalah yang membuat
+                'updated_by' => 1  // Asumsi user dengan ID 1 adalah yang terakhir mengupdate
+            ],
+            [
+                'nama_chamber' => 'Chamber 3',
+                'keterangan' => 'Keterangan untuk Chamber 3',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'created_by' => 1, // Asumsi user dengan ID 1 adalah yang membuat
+                'updated_by' => 1  // Asumsi user dengan ID 1 adalah yang terakhir mengupdate
+            ],
+            [
+                'nama_chamber' => 'Chamber 4',
+                'keterangan' => 'Keterangan untuk Chamber 4',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'created_by' => 1, // Asumsi user dengan ID 1 adalah yang membuat
+                'updated_by' => 1  // Asumsi user dengan ID 1 adalah yang terakhir mengupdate
+            ],
+            [
+                'nama_chamber' => 'Chamber 5',
+                'keterangan' => 'Keterangan untuk Chamber 5',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'created_by' => 1, // Asumsi user dengan ID 1 adalah yang membuat
+                'updated_by' => 1  // Asumsi user dengan ID 1 adalah yang terakhir mengupdate
+            ],
+            [
+                'nama_chamber' => 'Chamber 6',
+                'keterangan' => 'Keterangan untuk Chamber 6',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'created_by' => 1, // Asumsi user dengan ID 1 adalah yang membuat
+                'updated_by' => 1  // Asumsi user dengan ID 1 adalah yang terakhir mengupdate
+            ],
+            [
+                'nama_chamber' => 'Chamber 7',
+                'keterangan' => 'Keterangan untuk Chamber 7',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'created_by' => 1, // Asumsi user dengan ID 1 adalah yang membuat
+                'updated_by' => 1  // Asumsi user dengan ID 1 adalah yang terakhir mengupdate
+            ],
+            [
+                'nama_chamber' => 'Chamber 8',
+                'keterangan' => 'Keterangan untuk Chamber 8',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'created_by' => 1, // Asumsi user dengan ID 1 adalah yang membuat
+                'updated_by' => 1  // Asumsi user dengan ID 1 adalah yang terakhir mengupdate
+            ],
+            [
+                'nama_chamber' => 'Chamber 9',
+                'keterangan' => 'Keterangan untuk Chamber 9',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'created_by' => 1, // Asumsi user dengan ID 1 adalah yang membuat
+                'updated_by' => 1  // Asumsi user dengan ID 1 adalah yang terakhir mengupdate
+            ],
+            [
+                'nama_chamber' => 'Chamber 10',
+                'keterangan' => 'Keterangan untuk Chamber 10',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'created_by' => 1, // Asumsi user dengan ID 1 adalah yang membuat
+                'updated_by' => 1  // Asumsi user dengan ID 1 adalah yang terakhir mengupdate
+            ],
+            [
+                'nama_chamber' => 'Chamber 11',
+                'keterangan' => 'Keterangan untuk Chamber 11',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'created_by' => 1, // Asumsi user dengan ID 1 adalah yang membuat
+                'updated_by' => 1  // Asumsi user dengan ID 1 adalah yang terakhir mengupdate
+            ],
+            [
+                'nama_chamber' => 'Chamber 12',
+                'keterangan' => 'Keterangan untuk Chamber 12',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'created_by' => 1, // Asumsi user dengan ID 1 adalah yang membuat
+                'updated_by' => 1  // Asumsi user dengan ID 1 adalah yang terakhir mengupdate
+            ],
+            [
+                'nama_chamber' => 'Chamber 13',
+                'keterangan' => 'Keterangan untuk Chamber 13',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'created_by' => 1, // Asumsi user dengan ID 1 adalah yang membuat
+                'updated_by' => 1  // Asumsi user dengan ID 1 adalah yang terakhir mengupdate
+            ],
+            [
+                'nama_chamber' => 'Chamber 14',
+                'keterangan' => 'Keterangan untuk Chamber 14',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'created_by' => 1, // Asumsi user dengan ID 1 adalah yang membuat
+                'updated_by' => 1  // Asumsi user dengan ID 1 adalah yang terakhir mengupdate
+            ],
+            [
+                'nama_chamber' => 'Chamber 15',
+                'keterangan' => 'Keterangan untuk Chamber 15',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'created_by' => 1, // Asumsi user dengan ID 1 adalah yang membuat
+                'updated_by' => 1  // Asumsi user dengan ID 1 adalah yang terakhir mengupdate
+            ],
+            [
+                'nama_chamber' => 'Chamber 16',
+                'keterangan' => 'Keterangan untuk Chamber 16',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'created_by' => 1, // Asumsi user dengan ID 1 adalah yang membuat
+                'updated_by' => 1  // Asumsi user dengan ID 1 adalah yang terakhir mengupdate
+            ],
+            [
+                'nama_chamber' => 'Chamber 17',
+                'keterangan' => 'Keterangan untuk Chamber 17',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'created_by' => 1, // Asumsi user dengan ID 1 adalah yang membuat
+                'updated_by' => 1  // Asumsi user dengan ID 1 adalah yang terakhir mengupdate
+            ],
+            // Tambahkan lebih banyak data di sini jika diperlukan
+        ]);
+    }
+}

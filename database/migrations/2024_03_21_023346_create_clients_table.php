dd76a62fd223ab42963bb94f740d9b607c10de90
Doni Nurramdan <doni.nurramdan@indocyber.id>
2024-03-21 10:10:25 +0700

    create migration client

---
+++ database/migrations/2024_03_21_023346_create_clients_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_client');
+            $table->string('batas_atas');
+            $table->string('batas_bawah');
+            $table->text('keterangan')->nullable();
+            $table->timestamps(); // Ini akan membuat kolom created_at dan updated_at
+            $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('updated_by')->nullable()->constrained('users')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};

50b4216baa904866f92175f6bbde307dde4727b1
Doni Nurramdan <doni.nurramdan@indocyber.id>
2024-03-21 10:09:59 +0700

    create migration chamber

---
+++ database/migrations/2024_03_21_022637_create_chambers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chambers', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_chamber');
+            $table->text('keterangan')->nullable();
+            $table->timestamps(); // Ini akan membuat kolom created_at dan updated_at
+            $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('updated_by')->nullable()->constrained('users')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chambers');
+    }
+};

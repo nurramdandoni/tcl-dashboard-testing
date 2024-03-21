1a76eea02f2cedf292a7b047e3be3d0d005f93fa
Doni Nurramdan <doni.nurramdan@indocyber.id>
2024-03-21 12:10:13 +0700

    add dynamic data process with ajax

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -67,163 +67,268 @@
                     <canvas id="myPieChart"></canvas>
                 </div>
                 <hr>
-                Data Berdasarkan Total Status 'OK' pada semua chamber
+                Data dan Warna Berdasarkan Total Status 'OK' pada semua chamber
+                <br>
+                Hijau = OK > 96%
+                <br>
+                Merah = OK < 96%
                 <!-- Styling for the donut chart can be found in the
                 <code>/js/demo/chart-pie-demo.js</code> file. -->
             </div>
         </div>
     </div>
 </div>
-
+<meta name="csrf-token" content="{{ csrf_token() }}">
 </div>
 <!-- /.container-fluid -->
 <script>
-    // Pie Chart Example
-var ctx = document.getElementById("myPieChart");
-var myPieChart = new Chart(ctx, {
-  type: 'doughnut',
-  data: {
-    labels: ["Direct", "Referral", "Social"],
-    datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
-      hoverBorderColor: "rgba(234, 236, 244, 1)",
-    }],
-  },
-  options: {
-    maintainAspectRatio: false,
-    tooltips: {
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      caretPadding: 10,
-    },
-    legend: {
-      display: false
-    },
-    cutoutPercentage: 80,
-  },
-});
+    $(document).ready(function() {
+        // Buat objek tanggal baru
+        var today = new Date();
 
-// Area Chart Example
-var ctx = document.getElementById("myAreaChart");
-var myLineChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ["10:05", "10:10", "10:15", "10:20", "10:25", "10:30", "10:35", "10:35", "10:40", "10:45", "10:50", "10:55"],
-    datasets: [{
-      label: "Temperature",
-      lineTension: 0.3,
-      backgroundColor: "rgba(78, 115, 223, 0.05)",
-      borderColor: "rgba(78, 115, 223, 1)",
-      pointRadius: 3,
-      pointBackgroundColor: "rgba(78, 115, 223, 1)",
-      pointBorderColor: "rgba(78, 115, 223, 1)",
-      pointHoverRadius: 3,
-      pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
-      pointHoverBorderColor: "rgba(78, 115, 223, 1)",
-      pointHitRadius: 10,
-      pointBorderWidth: 2,
-      data: [-19, -12, -23, -20, -18, -15, -15, -16, -40, -25, -27, -55],
-    }],
-  },
-  options: {
-    maintainAspectRatio: false,
-    layout: {
-      padding: {
-        left: 10,
-        right: 25,
-        top: 25,
-        bottom: 0
-      }
-    },
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'date'
-        },
-        gridLines: {
-          display: false,
-          drawBorder: false
-        },
-        ticks: {
-          maxTicksLimit: 7
-        }
-      }],
-      yAxes: [{
-        ticks: {
-          maxTicksLimit: 5,
-          padding: 10,
-          // Include a dollar sign in the ticks
-          callback: function(value, index, values) {
-            return value;
-          }
-        },
-        gridLines: {
-          color: "rgb(234, 236, 244)",
-          zeroLineColor: "rgb(234, 236, 244)",
-          drawBorder: false,
-          borderDash: [2],
-          zeroLineBorderDash: [2]
-        }
-      }],
-    },
-    legend: {
-      display: false
-    },
-    tooltips: {
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      titleMarginBottom: 10,
-      titleFontColor: '#6e707e',
-      titleFontSize: 14,
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      intersect: false,
-      mode: 'index',
-      caretPadding: 10,
-      callbacks: {
-        label: function(tooltipItem, chart) {
-          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + ': ' + tooltipItem.yLabel+' °C';
-        }
-      }
-    },
-    // options lainnya...
-    annotation: {
-        annotations: [{
-            type: 'line',
-            mode: 'horizontal',
-            scaleID: 'y-axis-0',
-            value: -17,
-            borderColor: 'red',
-            borderWidth: 2,
-            label: {
-                enabled: true,
-                content: ' Max. -17°C'
+        // Dapatkan tahun, bulan, dan tanggal
+        var year = today.getFullYear();
+        var month = ('0' + (today.getMonth() + 1)).slice(-2); // Tambahkan 1 karena bulan dimulai dari 0
+        var day = ('0' + today.getDate()).slice(-2);
+
+        // Gabungkan tahun, bulan, dan tanggal dengan format yang diinginkan
+        var formattedDate = year + '-' + month + '-' + day;
+
+        console.log(formattedDate)
+        // Panggil AJAX di sini
+        $.ajax({
+            url: '/dataBar',
+            type: 'POST',
+            dataType: 'json',
+            data:{
+                tanggal:formattedDate+'%',
+                client_id:'1',
+                _token: $('meta[name="csrf-token"]').attr('content') // Menyertakan token CSRF
+            },
+            success: function(response) {
+                var list_time = [];
+                var list_temp = [];
+                var batas_atas_thresh = response[0].client.batas_atas;
+                var batas_bawah_thresh = response[0].client.batas_bawah;
+                // console.log(response[0]);
+                $.each(response, function(key, val){
+                    // console.log(val.created_at);
+                    var tempTime = val.created_at;
+                    var spliting = tempTime.split('T');
+                    var substring = spliting[1];
+                    var timeData = substring.substring(0,5);
+                    list_time.push(timeData);
+                    list_temp.push(val.temperature_data);
+                })
+                console.log(list_time);
+                console.log(list_temp);
+                // Tambahkan logika untuk menangani data dari response AJAX di sini
+                // Area Chart Example
+                var ctx = document.getElementById("myAreaChart");
+                var myLineChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    // labels: ["10:05", "10:10", "10:15", "10:20", "10:25", "10:30", "10:35", "10:35", "10:40", "10:45", "10:50", "10:55"],
+                    labels: list_time,
+                    datasets: [{
+                    label: "Temperature",
+                    lineTension: 0.3,
+                    backgroundColor: "rgba(78, 115, 223, 0.05)",
+                    borderColor: "rgba(78, 115, 223, 1)",
+                    pointRadius: 3,
+                    pointBackgroundColor: "rgba(78, 115, 223, 1)",
+                    pointBorderColor: "rgba(78, 115, 223, 1)",
+                    pointHoverRadius: 3,
+                    pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+                    pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+                    pointHitRadius: 10,
+                    pointBorderWidth: 2,
+                    // data: [-19, -12, -23, -20, -18, -15, -15, -16, -40, -25, -27, -55],
+                    data: list_temp,
+                    }],
+                },
+                options: {
+                    maintainAspectRatio: false,
+                    layout: {
+                    padding: {
+                        left: 10,
+                        right: 25,
+                        top: 25,
+                        bottom: 0
+                    }
+                    },
+                    scales: {
+                    xAxes: [{
+                        time: {
+                        unit: 'date'
+                        },
+                        gridLines: {
+                        display: false,
+                        drawBorder: false
+                        },
+                        ticks: {
+                        maxTicksLimit: 7
+                        }
+                    }],
+                    yAxes: [{
+                        ticks: {
+                        maxTicksLimit: 5,
+                        padding: 10,
+                        // Include a dollar sign in the ticks
+                        callback: function(value, index, values) {
+                            return value;
+                        }
+                        },
+                        gridLines: {
+                        color: "rgb(234, 236, 244)",
+                        zeroLineColor: "rgb(234, 236, 244)",
+                        drawBorder: false,
+                        borderDash: [2],
+                        zeroLineBorderDash: [2]
+                        }
+                    }],
+                    },
+                    legend: {
+                    display: false
+                    },
+                    tooltips: {
+                    backgroundColor: "rgb(255,255,255)",
+                    bodyFontColor: "#858796",
+                    titleMarginBottom: 10,
+                    titleFontColor: '#6e707e',
+                    titleFontSize: 14,
+                    borderColor: '#dddfeb',
+                    borderWidth: 1,
+                    xPadding: 15,
+                    yPadding: 15,
+                    displayColors: false,
+                    intersect: false,
+                    mode: 'index',
+                    caretPadding: 10,
+                    callbacks: {
+                        label: function(tooltipItem, chart) {
+                        var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                        return datasetLabel + ': ' + tooltipItem.yLabel+' °C';
+                        }
+                    }
+                    },
+                    // options lainnya...
+                    annotation: {
+                        annotations: [{
+                            type: 'line',
+                            mode: 'horizontal',
+                            scaleID: 'y-axis-0',
+                            value: batas_atas_thresh,
+                            borderColor: 'red',
+                            borderWidth: 2,
+                            label: {
+                                enabled: true,
+                                content: ' Max. '+batas_atas_thresh+'°C'
+                            }
+                        }, {
+                            type: 'line',
+                            mode: 'horizontal',
+                            scaleID: 'y-axis-0',
+                            value: batas_bawah_thresh,
+                            borderColor: 'green',
+                            borderWidth: 2,
+                            label: {
+                                enabled: true,
+                                content: 'Min. '+batas_bawah_thresh+'°C'
+                            }
+                        }]
+                    }
+                }
+                });
+
+                var status_ok =0;
+                var status_lower=0;
+                var status_higher =0;
+                var persentase_ok = 0
+                
+                $.each(response, function(key2, val2){
+                    // console.log(val2.client.batas_atas);
+                    // console.log(val2.temperature_data);
+                    if(val2.temperature_data < val2.client.batas_atas){
+                        status_higher++;
+                    }else if(val2.temperature_data > val2.client.batas_bawah){
+                        status_lower++;
+                    }else{
+                        status_ok++
+                    }
+                });
+                console.log("total data : ",response.length);
+                persentase_ok = (status_ok/response.length)*100;
+                console.log("presentase ok : ",persentase_ok);
+                var warna = [];
+                if(persentase_ok > 96){
+                    var warna = ['#59df4e', '#59df4e', '#59df4e'];
+                }else{
+                    var warna = ['#f21111', '#f21111', '#f21111'];
+                }
+                var list_status = ["OK","HIGHER","LOWER"];
+                var list_data = [status_ok,status_higher,status_lower];
+                // Pie Chart Example
+                var ctx = document.getElementById("myPieChart");
+                var myPieChart = new Chart(ctx, {
+                type: 'doughnut',
+                data: {
+                    // labels: ["Direct", "Referral", "Social"],
+                    labels: list_status,
+                    datasets: [{
+                    // data: [55, 30, 15],
+                    data: list_data,
+                    // backgroundColor: ['#59df4e', '#59df4e', '#59df4e'],
+                    backgroundColor: warna,
+                    hoverBackgroundColor: ['#59df4e', '#f21111', '#4e73df'],
+                    hoverBorderColor: "rgba(234, 236, 244, 1)",
+                    }],
+                },
+                options: {
+                    maintainAspectRatio: false,
+                    tooltips: {
+                    backgroundColor: "rgb(255,255,255)",
+                    bodyFontColor: "#858796",
+                    borderColor: '#dddfeb',
+                    borderWidth: 1,
+                    xPadding: 15,
+                    yPadding: 15,
+                    displayColors: false,
+                    caretPadding: 10,
+                    },
+                    legend: {
+                    display: false
+                    },
+                    // cutoutPercentage: 80,
+                    onClick: function(event, elements) {
+                    // Ambil elemen yang diklik
+                    var clickedElement = this.getElementAtEvent(event);
+
+                    // Jika ada elemen yang diklik
+                    if (clickedElement.length > 0) {
+                        // Ambil indeks elemen yang diklik
+                        var dataIndex = clickedElement[0]._index;
+
+                        // Ambil data sesuai dengan indeks
+                        var dataLabel = this.data.labels[dataIndex];
+                        var dataValue = this.data.datasets[0].data[dataIndex];
+
+                        // Tampilkan data
+                        console.log("Data Label: " + dataLabel);
+                        console.log("Data Value: " + dataValue);
+                    }
+                    }
+                },
+                });
+
+            },
+            error: function(xhr, status, error) {
+                console.error(xhr.responseText);
             }
-        }, {
-            type: 'line',
-            mode: 'horizontal',
-            scaleID: 'y-axis-0',
-            value: -23,
-            borderColor: 'green',
-            borderWidth: 2,
-            label: {
-                enabled: true,
-                content: 'Min. -23°C'
-            }
-        }]
-    }
-  }
-});
+        });
+    });
+
+
+
 </script>
 @endsection
